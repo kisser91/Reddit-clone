a95acfd41708965a66062a93bf8bbad59e38fd63
Nahuel Kisser <81387542+kisser91@users.noreply.github.com>
2022-05-22 23:50:14 -0300

    in progress

---
+++ components/PostBox.tsx
@@ -0,0 +1,198 @@
+import React from 'react'
+import { useSession } from 'next-auth/react'
+import Avatar from './Avatar'
+import { LinkIcon, PhotographIcon } from '@heroicons/react/outline'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ADD_POST, ADD_SUBREDDIT } from '../graphql/mutations'
+import client from '../apollo-client'
+import { GET_SUBREDDIT_BY_TOPIC } from '../graphql/queries'
+import toast from 'react-hot-toast'
+
+type FormData = {
+  postTitle: string
+  postBody: string
+  postImage: string
+  subreddit: string
+}
+
+function PostBox() {
+  const { data: session } = useSession()
+  const [addPost] = useMutation(ADD_POST)
+  const [addSubreddit] = useMutation(ADD_SUBREDDIT)
+  const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false)
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  const onSubmit = handleSubmit(async (formData) => {
+    console.log(formData)
+    const notification = toast.loading('Creating new post...')
+    console.log('asdas')
+
+    try {
+      //Query for the subreddit topic...
+      const {
+        data: { getSubredditListByTopic },
+      } = await client.query({
+        query: GET_SUBREDDIT_BY_TOPIC,
+        variables: {
+          topic: formData.subreddit,
+        },
+      })
+      const subredditExist = getSubredditListByTopic.length > 0
+      if (!subredditExist) {
+        // create the subreddit
+        console.log('Subreddit is new! -> creating a NEW subreddit! ')
+        const {
+          data: { insertSubreddit: newSubreddit },
+        } = await addSubreddit({
+          variables: {
+            topic: formData.subreddit,
+          },
+        })
+        console.log('Creating post...', formData)
+        const image = formData.postImage || ''
+
+        const {
+          data: { inserPost: newPost },
+        } = await addPost({
+          variables: {
+            body: formData.postBody,
+            image: image,
+            subreddit_id: newSubreddit.id,
+            title: formData.postTitle,
+            username: session?.user?.name,
+          },
+        })
+
+        console.log('New post added', newPost)
+      } else {
+        // use the existing subreddit...
+        console.log('Using existing subreddit!')
+        console.log(getSubredditListByTopic)
+
+        const image = formData.postImage || ''
+
+        const {
+          data: { insertPost: newPost },
+        } = await addPost({
+          variables: {
+            body: formData.postBody,
+            image: image,
+            subreddit_id: getSubredditListByTopic[0].id,
+            title: formData.postTitle,
+            username: session?.user?.name,
+          },
+        })
+
+        console.log('New post added', newPost)
+      }
+      // After the post has been added!
+      setValue('postBody', '')
+      setValue('postImage', '')
+      setValue('postTitle', '')
+      setValue('subreddit', '')
+
+      toast.success('New post Created!', {
+        id: notification,
+      })
+    } catch (error) {
+      toast.error('Whooops something went wrong', {
+        id: notification,
+      })
+    }
+  })
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="sticky top-16 z-50 rounded-md border bg-white p-2"
+    >
+      <div className="flex items-center space-x-3">
+        <Avatar />
+        <input
+          {...register('postTitle', { required: true })}
+          disabled={!session}
+          type="text"
+          className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
+          placeholder={
+            session ? 'Create a post by entering a title!' : 'Sign in to post'
+          }
+        />
+        <PhotographIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 cursor-pointer text-gray-300 ${
+            imageBoxOpen && 'text-blue-300'
+          }`}
+        />
+        <LinkIcon className={`h-6 cursor-pointer text-gray-300`} />
+      </div>
+
+      {!!watch('postTitle') && (
+        <div className="flex flex-col py-2">
+          {/* Body */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+              {...register('postBody')}
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              type="text"
+              placeholder="Text (Optional)"
+            />
+          </div>
+
+          {/* subreddit */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+            <input
+              {...register('subreddit', { required: true })}
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              type="text"
+              placeholder="i.e reactjs"
+            />
+          </div>
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+              <input
+                {...register('postImage')}
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                type="text"
+                placeholder="Optional..."
+              />
+            </div>
+          )}
+          {/* Errors */}
+          {Object.keys(errors).length > 0 && (
+            <div className="space-y-2 p-2 text-red-500">
+              {errors.postTitle?.type === 'required' && (
+                <p>- A Post Title is required</p>
+              )}
+
+              {errors.subreddit?.type === 'required' && (
+                <p>- A Subreddit is required</p>
+              )}
+            </div>
+          )}
+
+          {!!watch('postTitle') && (
+            <button
+              type="submit"
+              className="w-full rounded-full bg-blue-400 p-2 text-white"
+            >
+              Create Post
+            </button>
+          )}
+        </div>
+      )}
+    </form>
+  )
+}
+
+export default PostBox
